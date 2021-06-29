a46f92736480381d8533b1c7bdbfea8e0ac5bed2
grrttmrtn <empty>
2021-06-28 21:17:31 -0500

    added most functions of api

--- urlscanio.py
+++ urlscanio.py
@@ -3,19 +3,17 @@ import json
 
 
 class urlscanio:
-
     def __init__(self, api_key, visibility=None, base_uri=None):
         self.api_key = api_key
-        if visibility = None:
-            visibility = 'public'
+        if visibility == None:
+            self.visibility = 'public'
         else:
             self.visibility = visibility
-        if base_uri = None:
-            base_uri = 'https://urlscan.io/api/v1/'
+        if base_uri == None:
+            self.base_uri = 'https://urlscan.io/api/v1/'
         else:
             self.base_uri = self.checkBaseURI(base_uri)
         self.headers = {'API-Key': self.api_key,'Content-Type':'application/json'}
-    
     def checkBaseURI(uri):
         if uri(len(uri)-1) != '/':
             return uri + '/'
@@ -27,33 +25,36 @@ class urlscanio:
         elif process == 'dom':
             uri = 'https://urlscan.io/dom/' + uuid
         elif process == 'screenshots':
-            uri = 'https://urlscan.io/screenshots/' + uuid = '.png'
+            uri = 'https://urlscan.io/screenshots/' + uuid + '.png'
         i=0
+        print(uri)
         while i <= 60:
             try:
+                print(i)
                 response = requests.get(uri, headers=self.headers)
+                print(response)
                 if response.status_code == requests.codes.ok:
-                    return response
+                    return response.content
                     break
-            i += 1
+                if i == 60:
+                    return response.content
+                i+=1
             except Exception as e:
                 print(e)
-
     def submit(self, uri):
         try:
             data = {"url": uri, "visibility": self.visibility}
             response = requests.post(self.base_uri + 'scan/',headers=self.headers, data=json.dumps(data))
-            if response.status_code = requests.codes.ok:
-                print(json.dumps(response.contentm indent=2))
+            if response.status_code == requests.codes.ok:
+                return response.json()
             else:
                 response.raise_for_status()
         except Exception as e:
             print(e)
-
     def getDom(self, uuid):
-
+        return self.waitForResults('dom', uuid)
     def getScreenshots(self, uuid):
-    
+        return self.waitForResults('screenshots', uuid)   
     def getResults(self, uuid):
-
+        return self.waitForResults('result', uuid)
     def search(self, search):
